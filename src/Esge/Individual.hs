eb5cc64c510936436a37bdc7035dd7a93aa541ed
Simon Goller <neosam+github@posteo.de>
2016-02-19 21:00:12 +0100

    Removed health and mana from individual

--- src/Esge/Individual.hs
+++ src/Esge/Individual.hs
@@ -11,7 +11,7 @@ Add Individual type and functions to the game.
 -}
 
 module Esge.Individual (
-            Individual(Individual, key, name, desc, health, mana, items),
+            Individual(Individual, key, name, desc, items),
             allIndividuals,
             getIndividual,
             getIndividualNull,
@@ -25,20 +25,13 @@ data Individual = Individual {
     key :: String,
     name :: String,
     desc :: String,
-    health :: (Int, Int),
-    mana :: (Int, Int),
     items :: [(String, String)]
 } deriving (Show, Read, Eq)
 
 -- | Used in case of errors
 nullIndividual :: Individual
-nullIndividual = Individual "nullIndividual" "" "" (0, 0) (0, 0) []
+nullIndividual = Individual "nullIndividual" "" "" []
 
--- | Merge a pair of Int to a String
-mergeIntPair :: String      -- ^ Separator between the numbers
-            -> (Int, Int)   -- ^ Ints to merge
-            -> String       -- ^ Generated String
-mergeIntPair sep (a, b) = show a ++ sep ++ show b
 
 -- | Merge a pair of Strings
 mergeStringPair :: String               -- ^ Separator
@@ -51,18 +44,11 @@ strToStrPair :: String -> (String, String)
 strToStrPair str = let (first, second) = break (== ',') str in
     (first, tail second)
 
--- | Split comma separaed Ints to Int pair
-strToIntPair :: String -> (Int, Int)
-strToIntPair str = (read a, read b)
-    where (a, b) = strToStrPair str
-
 -- | Individual can be stored in 'EC.Ingame'
 instance EC.Storageable Individual where
     toStorage ind = EC.Storage (key ind) "individual" [
             ("name", name ind),
             ("desc", desc ind),
-            ("health", mergeIntPair "," $ health ind),
-            ("mana", mergeIntPair "," $ mana ind),
             ("items", unwords $ map (mergeStringPair ",") $ items ind)
         ]
     fromStorage (EC.Storage stKey t metas) =
@@ -70,15 +56,11 @@ instance EC.Storageable Individual where
         else do
             stName <- lookup "name" metas
             stDesc <- lookup "desc" metas
-            stHealth <- lookup "health" metas
-            stMana <- lookup "mana" metas
             stItems <- lookup "items" metas
             return Individual {
                 key = stKey,
                 name = stName,
                 desc = stDesc,
-                health = strToIntPair stHealth,
-                mana = strToIntPair stMana,
                 items = map strToStrPair $ words stItems
             }
 
